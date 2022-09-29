0a4fb07a4864f9f3aa5242137311a667bfe8c62a
Dnwawuzie <dnwawuzie@deloitte.ca>
2022-09-29 11:18:49 -0400

    major changes

---
+++ examples/iam-user/variables.tf
@@ -0,0 +1,28 @@
+variable "team" {
+  description = "Team name"
+  type        = string
+}
+
+# variable "region" {
+#   type = string
+# }
+
+# variable "tags" {
+#   description = "A map of tags to add to all resources."
+#   type        = map(string)
+# }
+
+# variable "dynamo_table" {
+#   description = "terraform backend configuration"
+#   type        = string
+# }
+
+# variable "bucket" {
+#   description = "terraform backend configuration"
+#   type        = string
+# }
+
+# variable "account_id" {
+#   description = "terraform backend configuration"
+#   type        = string
+# }
